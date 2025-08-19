569b5362e7aecf8c2056d3593901f7a0c50f56cd
Caleb Hamernick <95885153+Hamernick@users.noreply.github.com>
2025-08-19 17:00:07 -0400

    feat: enhance contacts data table

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,16 +1,15 @@
 import { SidebarInset } from "@/components/ui/sidebar"
 import { SiteHeader } from "@/components/site-header"
 
-import { columns } from "./columns"
 import { contacts } from "./data"
-import { DataTable } from "./data-table"
+import { ContactsDataTable } from "./contacts-data-table"
 
 export default function ContactsPage() {
   return (
     <SidebarInset>
       <SiteHeader title="Contacts" />
       <div className="p-4">
-        <DataTable columns={columns} data={contacts} />
+        <ContactsDataTable data={contacts} />
       </div>
     </SidebarInset>
   )
