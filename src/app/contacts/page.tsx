f0061b81b8bb6d9c5ad4e4bd779ee583805a8106
Caleb Hamernick <95885153+Hamernick@users.noreply.github.com>
2025-08-19 16:28:13 -0400

    Add contacts data table with dummy data

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,11 +1,16 @@
 import { SidebarInset } from "@/components/ui/sidebar"
 import { SiteHeader } from "@/components/site-header"
 
+import { columns } from "./columns"
+import { contacts } from "./data"
+import { DataTable } from "./data-table"
+
 export default function ContactsPage() {
   return (
     <SidebarInset>
       <SiteHeader title="Contacts" />
       <div className="p-4">
+        <DataTable columns={columns} data={contacts} />
       </div>
     </SidebarInset>
   )
